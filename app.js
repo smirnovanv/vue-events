41a05a0b94a7d41d2c1a02b307c0afc15e61d5cb
smirnovanv <smirnovanv89@gmail.com>
2022-03-24 00:34:50 +0300

    feat: add form submition

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const app = Vue.createApp({
     };
   },
   methods: {
+    submitForm(event) {
+      event.preventDefault();
+      alert('submitted');
+    },
     add(num) {
       this.counter = this.counter + num;
     },
